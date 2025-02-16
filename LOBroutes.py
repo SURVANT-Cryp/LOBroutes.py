f41cb31c76dcd3f363e82a6677bf7c1f30d4d355
patrick quinn <SURVANT-Cryp@users.noreply.github.com>
2025-02-16 16:39:26 -0500

    Add files via upload

--- LOBroutes.py
+++ LOBroutes.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 # coding: utf-8
-#“Like all men of the Library, I have traveled in my youth. I have journeyed in search of a book, perhaps of the catalogue of catalogues; now that my eyes can scarcely decipher what I write, I am preparing to die a few leagues from the hexagon in which I was born. Once dead, there will not lack pious hands to hurl me over the banister; my sepulchre shall be the unfathomable air: my body will sink lengthily and will corrupt and dissolve in the wind engendered by the fall, which is infinite.” 
+#“This much is already known: for every sensible line of straightforward statement, there are leagues of senseless cacophonies, verbal jumbles and incoherences. (I know of an uncouth region whose librarians repudiate the vain and superstitious custom of finding a meaning in books and equate it with that of finding a meaning in dreams or in the chaotic lines of one’s palm.” 
 #“De Quincey describes using the stars to guide him home. Having no knowledge of celestial navigation he finds himself in unfamiliar territories, discovering what he believes are streets anonymous to maps, thus reimagining the city in his own eyes.” 
 
 import string
 import random
 import decimal
-import textwrap
 import sys
 import time
+import textwrap
 from random import randint
 import webbrowser
+
 
 def slowprint(s):
     for c in s + '\n':
@@ -33,5 +34,7 @@ urls = ['https://www.google.com/maps']
 
 for url in urls:
     webbrowser.open_new_tab(url)
+
+
 
 
